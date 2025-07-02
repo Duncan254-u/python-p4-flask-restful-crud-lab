dfbca9b3526dfa7ca1d31d6d0b1e9f85f3c55c48
Duncan Kibet <kibet2704@gmail.com>
2025-07-02 14:44:03 +0300

    run final tests

--- server/app.py
+++ server/app.py
@@ -1,53 +1,48 @@
+#!/usr/bin/env python3
 from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
 import os
 
+from models import db, Plant
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///plants.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
-CORS(app)
 
-class Plant(db.Model):
-    __tablename__ = 'plants'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    image = db.Column(db.String(200), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    is_in_stock = db.Column(db.Boolean, nullable=False, default=True)
-    
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'image': self.image,
-            'price': self.price,
-            'is_in_stock': self.is_in_stock
-        }
-
-# Routes
-
-# GET all plants
 @app.route('/plants', methods=['GET'])
 def get_plants():
     plants = Plant.query.all()
     return jsonify([plant.to_dict() for plant in plants])
 
-# GET single plant
-@app.route('/plants/<int:id>', methods=['GET'])
-def get_plant(id):
+# FIXED: Combined GET and PATCH into single route
+@app.route('/plants/<int:id>', methods=['GET', 'PATCH'])
+def plant_by_id(id):
     plant = Plant.query.get_or_404(id)
-    return jsonify(plant.to_dict())
+    
+    if request.method == 'GET':
+        return jsonify(plant.to_dict())
+    
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        
+        # Update only the fields that are provided in the request
+        if 'name' in data:
+            plant.name = data['name']
+        if 'image' in data:
+            plant.image = data['image']
+        if 'price' in data:
+            plant.price = data['price']
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+        
+        db.session.commit()
+        
+        return jsonify(plant.to_dict())
 
-# POST new plant
 @app.route('/plants', methods=['POST'])
 def create_plant():
     data = request.get_json()
@@ -64,27 +59,6 @@ def create_plant():
     
     return jsonify(new_plant.to_dict()), 201
 
-# PATCH update plant
-@app.route('/plants/<int:id>', methods=['PATCH'])
-def update_plant(id):
-    plant = Plant.query.get_or_404(id)
-    data = request.get_json()
-    
-    # Update only the fields that are provided in the request
-    if 'name' in data:
-        plant.name = data['name']
-    if 'image' in data:
-        plant.image = data['image']
-    if 'price' in data:
-        plant.price = data['price']
-    if 'is_in_stock' in data:
-        plant.is_in_stock = data['is_in_stock']
-    
-    db.session.commit()
-    
-    return jsonify(plant.to_dict())
-
-# DELETE plant
 @app.route('/plants/<int:id>', methods=['DELETE'])
 def delete_plant(id):
     plant = Plant.query.get_or_404(id)
@@ -94,15 +68,6 @@ def delete_plant(id):
     
     # Return empty response with 204 status code
     return make_response('', 204)
-
-# Error handlers
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({'error': 'Plant not found'}), 404
-
-@app.errorhandler(400)
-def bad_request(error):
-    return jsonify({'error': 'Bad request'}), 400
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,16 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-class Plant(db.Model, SerializerMixin):
+db = SQLAlchemy(metadata=metadata)
+
+class Plant(db.Model):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    price = db.Column(db.Numeric(8, 2))
+    is_in_stock = db.Column(db.Boolean, default=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': float(self.price) if self.price else None,
+            'is_in_stock': self.is_in_stock
+        }
 
     def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+        return f'<Plant {self.name}>'
